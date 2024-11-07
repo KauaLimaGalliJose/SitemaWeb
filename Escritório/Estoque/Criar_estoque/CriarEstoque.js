90584d77a5971c92f1c06fcae5bb0661ede2bf13
Kauasite <kaualimagallik@gmail.com>
2024-11-07 00:14:09 -0300

    07/11

--- Escritório/Estoque/Criar_estoque/CriarEstoque.js
+++ Escritório/Estoque/Criar_estoque/CriarEstoque.js
@@ -1,21 +1,39 @@
+let mRainha = document.getElementById('modelo_rainha');
+let preview =  document.getElementById('modelo_imagem');
+
 function img_modelo(){
     const fileInput = document.getElementById('ImagemInput');
-    const mRainha = document.getElementById('modelo_rainha');
  
     fileInput.addEventListener('change', function() {
         const file = this.files[0];
         
         if (file) {
             const reader = new FileReader();
-            let preview =  document.getElementById('modelo_imagem');
 
             reader.onload = function(event) {
                 preview.src = event.target.result;
                 preview.style.display = 'block'; // Mostra a imagem de pré-visualização
-                mRainha.style.display = 'none'
+                mRainha.style.display = 'none';
             }
 
             reader.readAsDataURL(file); // Lê o arquivo como uma URL de dados
         }
     });
+}
+
+function verificar(){
+    let chave = true;
+
+    while(chave == true){
+        if(preview.style.display !== 'block'){
+            chave = false;
+            alert('Sem Imagem ');
+        }
+        else{
+            
+        }
+        if(chave === true){
+            break
+        }
+    }
 }
