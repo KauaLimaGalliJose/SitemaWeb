e1dc7c4ee0233985cdc33820cf97c3808454b452
Kauasite <kaualimagallik@gmail.com>
2024-11-27 03:20:40 -0300

    a
    a

---
+++ phpinfo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="coroa.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servidor Informação</title>
+</head>
+<body>
+    <h1>Servidor Informação</h1>
+    <?php
+        phpinfo();
+        ?>
+</body>
+</html>
