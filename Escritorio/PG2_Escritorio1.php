8942292485ef012ecca83270e3165088a1099410
Kauasite <kaualimagallik@gmail.com>
2025-02-10 22:15:29 -0300

    1
    1

--- Escritorio/PG2_Escritorio1.php
+++ Escritorio/PG2_Escritorio1.php
@@ -1,6 +1,14 @@
  <?php
-    var_dump($_POST);
-    print('OK');
+    $local = "localhost";
+    $nomeBanco = "sprint3";
+    $user = 'root';
+    $senha = '';
+
+   $mySQL = new mysqli($local,$nomeBanco,$senha,$user);
+
+   if($mySQL->connect_errno){
+        echo 'Falha ao se Conectar: (' . $mySQL->connect_errno . ")" . $mySQL->connect_error;
+   }
 
 ?>
 
