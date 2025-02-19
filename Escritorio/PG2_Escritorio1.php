cb8f54380f5a0c78fde537a6c15ce4d2af1d5843
KauaLimaGalli1 <kaualimagallik@gmail.com>
2025-02-19 10:40:33 -0300

    19/02/2025

--- Escritorio/PG2_Escritorio1.php
+++ Escritorio/PG2_Escritorio1.php
@@ -21,17 +21,25 @@ $gravacao_exter = $_POST['gravacao_exter'];
 $gravacao_inter = $_POST['gravacao_inter'];
 $largura = $_POST['largura'];
 $outrosClientes = $_POST['txtcliente'];
-$semPedra = isset($_POST['semPedra']) ? $_POST['semPedra'] : false;
-$comPedra = isset($_POST['comPedra']) ? $_POST['comPedra'] : false;
-$estoqueFeminina = isset($_POST['estoqueFeminina']) ? $_POST['estoqueFeminina'] : false;
-$estoqueMasculina = isset($_POST['estoqueMasculina']) ? $_POST['estoqueMasculina'] : false;
+$semPedra = isset($_POST['semPedra']) ? $_POST['semPedra'] : null;
+$comPedra = isset($_POST['comPedra']) ? $_POST['comPedra'] : null;
+$estoqueFeminina = isset($_POST['estoqueFeminina']) ? $_POST['estoqueFeminina'] : null;
+$estoqueMasculina = isset($_POST['estoqueMasculina']) ? $_POST['estoqueMasculina'] : null;
 
 $idPedidos = $numeroPedido ."-". $dataEntrega;
 
+if($cliente == 'Centro_Alianca'){
+    $idCentroAlianca = $cliente . '-' . $nomePedidoC;
+    $idPedidos = $id;
+}
+if($cliente == 'Outros'){
+    $idOutros = $outrosClientes . '-' . $nomePedidoC;
+    $idPedidos =  $idOutros;
+}
 
 // passando pro banco de dados
 
 $dados = mysqli_query($conn, "INSERT INTO pedidos 
-    (idpedidos, cliente, nomePedido, numF, numeM, descricaoPedido, descricaoAlianca,largura, gravacaoInterna, gravacaoExterna,outrosClientes,imagem,parComEstoque,parSemEstoque,parPedra,parSemPedra) 
-    VALUES ('$idPedidos', '$cliente', '$nomePedido', '$f', '$m', '$descricao_Pedido', '$descricaoAlianca','$largura', '$gravacao_inter', '$gravacao_exter', '$outrosClientes','$imagem' , '$estoqueFeminina' ,'$$estoqueMasculina', '$semPedra' , '$comPedra' )");
+    (idpedidos, cliente, nomePedido, numF, numeM, descricaoPedido, descricaoAlianca,largura, gravacaoInterna, gravacaoExterna,outrosClientes,imagem,parEstoqueF,parEstoqueM,parPedra,parSemPedra) 
+    VALUES ('$idPedidos', '$cliente', '$nomePedido', '$f', '$m', '$descricao_Pedido', '$descricaoAlianca','$largura', '$gravacao_inter', '$gravacao_exter', '$outrosClientes','$imagem' , '$estoqueFeminina' ,'$estoqueMasculina', '$semPedra' , '$comPedra' )");
 ?>
