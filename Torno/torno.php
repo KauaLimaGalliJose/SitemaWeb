81dc4c1705ade1bf4daab7cdc45a31c4dba0604e
Kauasite <kaualimagallik@gmail.com>
2025-02-21 20:23:06 -0300

    21/02

--- Torno/torno.php
+++ Torno/torno.php
@@ -8,9 +8,28 @@
 </head>
 <body>
     <main>
-        <div id="cabecalho">
-          
+    <div id="cabecalho">
+        <div id="cabecalho_menu">
+            <div id="casa">
+                <button type="button" value=""  class="botao" >
+                <a href="../index.html"><img class="itens" src="../Escritorio/casa.png"></a>
+                </button>
+            </div>
+                <select id="larguraSelect">
+                        <option value="todos">Todos</option>
+                        <option value="2mm">2mm</option>
+                        <option value="3mm">3mm</option>
+                        <option value="4mm">4mm</option>
+                        <option value="5mm">5mm</option>
+                        <option value="6mm">6mm</option>
+                        <option value="7mm">7mm</option>
+                        <option value="8mm">8mm</option>
+                        <option value="9mm">9mm</option>
+                        <option value="10mm">10mm</option>
+                </select>
         </div>
+
+    </div>
     </main>
     <?php 
     include_once('../conexao.php');
